4b8e3bedd01ed3f0a59d7d46d9d9d2902a573151
Selma Baldeh <116042160+selma2232@users.noreply.github.com>
2026-09-06 23:44:50 +0300

    Fix production database connection

--- ict-jobs-finland/server/src/lib/prisma.ts
+++ ict-jobs-finland/server/src/lib/prisma.ts
@@ -1,8 +1,11 @@
 import { PrismaClient } from "../generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
-const connectionString =
-  "postgresql://postgres:postgres@localhost:51214/template1";
+const connectionString = process.env.DATABASE_URL;
+
+if (!connectionString) {
+  throw new Error("DATABASE_URL is not defined");
+}
 
 const adapter = new PrismaPg({
   connectionString,
