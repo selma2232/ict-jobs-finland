59553979a5178914c8999c96b24e20b20d340db6
Selma Baldeh <116042160+selma2232@users.noreply.github.com>
2026-09-06 23:57:00 +0300

    Update seed.ts

--- ict-jobs-finland/server/prisma/seed.ts
+++ ict-jobs-finland/server/prisma/seed.ts
@@ -3,8 +3,11 @@ import "dotenv/config";
 import { PrismaClient } from "../src/generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
-const connectionString =
-  "postgresql://postgres:postgres@localhost:51214/template1";
+const connectionString = process.env.DATABASE_URL;
+
+if (!connectionString) {
+  throw new Error("DATABASE_URL is not defined");
+}
 
 const adapter = new PrismaPg({
   connectionString,
@@ -13,8 +16,6 @@ const adapter = new PrismaPg({
 const prisma = new PrismaClient({
   adapter,
 });
-
-
 
 async function main() {
   console.log("Connecting to database...");
